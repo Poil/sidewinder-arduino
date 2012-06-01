f2f41fc3604483dd1b76b12432fa8d24e714db1e
saldsj3@gmail.com <saldsj3@gmail.com@cb4d1061-33b4-4473-5c78-c7c9ddf58c18>
2012-06-01 05:25:12 +0000

    got an effect working, but it seems finicky

--- trunk/ffb.h
+++ trunk/ffb.h
@@ -268,6 +268,7 @@ void FfbSendData(uint8_t *data, uint16_t len);
 
 //this function wasn't in header before
 void FfbSendSysEx(void *midi_data, uint16_t len);
+void FfbSendEffectOper(uint8_t effectId, uint8_t operation);
 
 // Debugging
 //	<index> should be pointer to an index variable whose value should be set to 0 to start iterating.
