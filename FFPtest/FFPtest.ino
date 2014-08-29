d073a37d19172a473ee9c0d5d337e120ac1deb6a
saldsj3@gmail.com <saldsj3@gmail.com@cb4d1061-33b4-4473-5c78-c7c9ddf58c18>
2014-08-29 05:06:25 +0000

    added vibration and constantforces as defaults to joystick, also added ability to read buttons.  Looking into why I can't produce as strong forces as I feel in fedit, I think it has to do with attack.

--- FFPtest/FFPtest.ino
+++ FFPtest/FFPtest.ino
@@ -2,25 +2,49 @@
 
 
 FFPJoystick stick;
+uint8_t magnitude = 20;
+uint8_t vibration;
 
 void setup()
 {
 
 //  stick.EnableAutoCenter();
-stick.DisableAutoCenter();
-stick.CreateConstantForce(20,0);
+//stick.DisableAutoCenter();
  Serial.begin(115200);
+// stick.CreateConstantForce(0,0);
 }
 void loop()
 {
   stick.Poll();
-//  Serial.println(stick.Throttle);
+/*  Serial.print(stick.effectId);
+  Serial.print(" ");
+  Serial.print(stick.constantForceID);  
+  Serial.print(stick.Throttle);
+  Serial.print(" ");
+  for(int i=0;i<9;i++)
+  {
+  Serial.print(stick.getButton(i));
+  Serial.print(" ");
+  }
+  Serial.println(stick.Button,BIN);*/
+  
+  vibration = (126 - (stick.Throttle + 63));
+  Serial.print(magnitude);
+  Serial.print(" ");
+  Serial.println(vibration);
+  
+  if(stick.getButton(0))
+    magnitude++;
+  if(stick.getButton(1))
+    magnitude--;  
+    stick.sendVibration(magnitude,vibration);
 //  stick.forceTest(60);
   delay(50);
-  if(stick.Throttle > 0)
-    stick.updateConstantForce(abs(stick.Throttle),0);
+/* if(stick.Throttle > 0)
+   stick.sendConstantForce(abs(stick.Throttle),0);
   else
-    stick.updateConstantForce(abs(stick.Throttle),0x3401);
+    stick.sendConstantForce(abs(stick.Throttle),0x3401);*/
+
 //  stick.playEffect(2);
 }
 
